c5207a4ddb85cf0cabffa191584aef653b4379f0
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-02-04 22:19:27 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function stringChop(str, size) {
     // your code here
-    let result = [];
-    for (let i = 0; i < str.length; i += size) {
-      result.push(str.slice(i, i + size));
-    }
-    return (result);
+    if(str.length > 0){
+		let result = [];
+	    for (let i = 0; i < str.length; i += size) {
+	      result.push(str.slice(i, i + size));
+	    };
+		
+		return result;
+	} else {
+		return [];
+	}
+
 }
 
 // Do not change the code below
